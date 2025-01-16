2645679380c030e88192e6dba4c57b8d6d4c4968
joe4780 <muthini181@gmail.com>
2025-01-16 12:08:02 +0300

    m

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -27,35 +27,29 @@ export default {
 </script>
 
 <style scoped>
-/* Add Google Font link for Poppins */
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap');
-
 .navbar {
   display: flex;
   justify-content: center;
   background: #119ab3;
   padding: 0px;
   font-weight: bold;
-  font-family: 'Poppins', sans-serif; /* Using the Poppins font */
+  font-family: 'Poppins', sans-serif;
 }
-
 ul {
   list-style: none;
   display: flex;
-  gap: 30px; /* Increased space between items */
+  gap: 20px;
 }
-
 li {
   color: #0c0404;
-  font-size: 26px; /* Increased font size */
+  font-size: 16px;
 }
-
 a {
   text-decoration: none;
   color: #070101;
 }
-
 a:hover {
   color: #00d853;
 }
-</style>
+</style>  
